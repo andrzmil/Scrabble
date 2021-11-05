3cd109890bcf5072fa17bfaa10f9e8febd659d44
andrzmil <andrz.milewski@gmail.com>
2021-11-05 18:45:41 +0100

    added beta multiple turns

--- GUI.py
+++ GUI.py
@@ -22,15 +22,18 @@ root.title("SCRABBLE")
 
 
 class Field():
-    def __init__(self, posx, posy, bg, bonus, state):
+    #generate field, set position x, position y, background color, bonus points, bonus type
+    def __init__(self, posx, posy, bg, bonus, bonus_type):
         self.posx = posx
         self.posy = posy
         self.bonus = bonus
-        self.state = state
+        self.bonus_type = bonus_type
+        #generate text variable and button, put on desired place
         self.txtvar = StringVar()
         self.btn = Button(root, text="", width="4", bg=bg, textvariable=self.txtvar)
         self.btn['command'] = lambda:[self.on_click_btn()]
         self.btn.grid(row=posx,column=posy)
+
 
     def on_click_btn(self):
         #count blanks
@@ -39,18 +42,16 @@ class Field():
         if_move_possible = Counter([B.board[self.posx-2][self.posy-1],B.board[self.posx][self.posy-1],
         B.board[self.posx-1][self.posy-2],B.board[self.posx-1][self.posy]])
 
-
-
+        #check the following conditions:
+        #text variable is blank, variable letter in use contains letter,
+        #at least one neighbouring field should be filled or number of blank fields should be 225 (state when game starts)
+        #additionally - direction x and y is filled in order to gather information about possible axis
         if self.txtvar.get() == "" and B.liu is not None and (if_move_possible.get('') != 4 or bl == 225):
-
             self.txtvar.set(B.liu)
             B.tmp_board[self.posx-1][self.posy-1] = B.liu
             B.directionx.append(self.posx-1)
             B.directiony.append(self.posy-1)
             B.liu = None
-            print(B.board)
-            print(B.directionx)
-            print(B.directiony)
         else:
             if B.liu is None and self.txtvar.get() != "":
                 B.tmp_board[self.posx-1][self.posy-1] = ''
@@ -123,6 +124,10 @@ class Player_Field():
         self.btn['command'] = lambda:[self.on_click_lt()]
         self.btn.grid(row=17,column=posy)
 
+    def on_destroy(self):
+        self.btn.destroy()
+        
+
     def on_click_lt(self):
         #if field is not blank - get letter
 
@@ -139,35 +144,51 @@ class Player_Field():
 
 class Player(Player_Field):
     def __init__(self,name,score=0,pos=None):
+        self.pos = pos
         self.name = name
         self.score = score
-        self.letters_poss = list()
+        self.letters_poss = []
         self.tmp_letter = []
         self.pl_btn_fld = []
+        #generate score indicators for all players
         self.pl_lbl_txt_var = StringVar()
         self.pl_lbl_txt_var.set(self.name + ': ' + str(self.score))
         self.pl_lbl = Label(root, textvariable=self.pl_lbl_txt_var)
-        self.pl_lbl.grid(row=pos, column=17)
+        self.pl_lbl.grid(row=self.pos, column=17)
 
-        global pl_dropped_letters
+        
 
 
     def generate_pl_let(self):
-        pl_label = Label(text=self.name)
-        pl_label.grid(row=16,column=1, columnspan=3)
+        self.pl_label = Label(root, text=self.name)
+        self.pl_label.grid(row=16,column=1, columnspan=3)
         for k in range(1,8):
-            tmp_pl_field = Player_Field(k, 'white', self.letters_poss[k-1])
-            self.pl_btn_fld.append(tmp_pl_field)
+            self.tmp_pl_field = Player_Field(k, 'white', self.letters_poss[k-1])
+            self.pl_btn_fld.append(self.tmp_pl_field)
         
-        self.pl_end_btn = Button(text='KONIEC TURY')
+        self.pl_end_btn = Button(root, text='KONIEC TURY', command=lambda:[self.destroy_pl_let()])
         self.pl_end_btn.grid(row=17,column=9, columnspan=4)
 
-        self.pl_chkbox = Checkbutton(text="WYMIANA")
+        self.pl_chkbox = Checkbutton(root, text="WYMIANA")
         self.pl_chkbox.grid(row = 17, column=13, columnspan=4)
 
     def destroy_pl_let(self):
+        #destroy all widgets created for current player and change to the next player
         self.pl_chkbox.destroy()
         self.pl_end_btn.destroy()
+        self.pl_label.destroy()
+        for wdg in self.pl_btn_fld:
+            wdg.on_destroy()
+
+    def new_turn(self):
+        if G.curr_player == G.no_players -1:
+            G.curr_player = 0
+            G.turn_state = True
+            G.curr_turn += 1
+        else:
+            G.curr_player += 1
+            G.turn_state = True
+            G.curr_turn += 1
         
         
         
@@ -186,34 +207,55 @@ B = Board()
 
 class Game():
     def __init__(self,no_players,turn_state=False):
+        self.game_state = False
         self.turn_state = False
         self.pl_pool = []
         self.no_players = no_players
         self.curr_player = 0
         self.curr_word = []
-        for i in range(int(no_players)):
-            pl_name = simpledialog.askstring(prompt='Podaj imię gracza nr ' + str(i+1), title='Do dzieła!')
-            tmp_player = Player(name=pl_name, pos=i+1)
-            self.pl_pool.append(tmp_player)
-        np.random.shuffle(self.pl_pool)
+        self.curr_turn = 0
 
 
+        #create players for testing purposes
+        tmp_player = Player(name="Andrzej", pos=1)
+        self.pl_pool.append(tmp_player)
+        tmp_player = Player(name="Adam", pos=2)
+        self.pl_pool.append(tmp_player)
+
+        
+
+        # for i in range(int(no_players)):
+        #     pl_name = simpledialog.askstring(prompt='Podaj imię gracza nr ' + str(i+1), title='Do dzieła!')
+        #     tmp_player = Player(name=pl_name, pos=i+1)
+        #    self.pl_pool.append(tmp_player)
+        
+    def players_turn(self):
+        #check if number of letters is 0
+        if len(self.pl_pool[self.curr_player].letters_poss) == 0:
+            self.pl_pool[self.curr_player].letters_poss = B.get_letters(7)
+            self.pl_pool[self.curr_player].generate_pl_let()
+        else:
+            self.pl_pool[self.curr_player].generate_pl_let()
             
 
+        
+            
+        
+        
+   
+        
 
-        self.pl_pool[self.curr_player].letters_poss = B.get_letters(7)
-        self.pl_pool[self.curr_player].generate_pl_let()
-        a = simpledialog.askstring(title='aaaa', prompt='aaaaa')
-        self.pl_pool[self.curr_player].destroy_pl_let()
+       
+        
         
 
 G = Game(2)
 
+G.players_turn()
 
 
-# P = Player("Zbyszek",pos=1)
-# P.generate_pl_let()
-# P.letters_poss = B.get_letters(7)
+
+
 
 if __name__ == "__main__":
     root.mainloop()
